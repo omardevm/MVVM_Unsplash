0fc4e4b3bfd6f0183d05d2758ef204ba0ceff606
omardevm <omardguez.contacto@gmail.com>
2021-04-08 19:01:40 -0500

    GalleryDetail basis

--- app/src/main/java/com/omar/mvvm/Views/Fragments/Gallery/GalleryFragment.kt
+++ app/src/main/java/com/omar/mvvm/Views/Fragments/Gallery/GalleryFragment.kt
@@ -9,6 +9,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.paging.LoadState
+import com.omar.mvvm.Models.Response.SearchPhotos.UnsplashPhoto
 import com.omar.mvvm.R
 import com.omar.mvvm.Utils.PagingPhotoAdapter
 import com.omar.mvvm.Utils.UnsplashPhotoLoadStateAdapter
@@ -16,7 +17,8 @@ import com.omar.mvvm.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+class GalleryFragment : Fragment(R.layout.fragment_gallery),
+    PagingPhotoAdapter.OnItemClickListener {
     private val viewModel by viewModels<GalleryViewModel>()
 
     private var _binding: FragmentGalleryBinding? = null
@@ -25,7 +27,7 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentGalleryBinding.bind(view)
-        val adapter = PagingPhotoAdapter()
+        val adapter = PagingPhotoAdapter(this)
         binding.apply {
             unsplashRecycler.setHasFixedSize(true)
             unsplashRecycler.itemAnimator = null
@@ -86,5 +88,9 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onItemClick(photo: UnsplashPhoto) {
+
     }
 }
---
+++ app/src/main/java/com/omar/mvvm/Views/Fragments/GalleryDetail/GalleryDetailFragment.kt
@@ -0,0 +1,11 @@
+package com.omar.mvvm.Views.Fragments.GalleryDetail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.omar.mvvm.R
+
+class GalleryDetailFragment : Fragment(R.layout.fragment_gallery_detail) {
+}
